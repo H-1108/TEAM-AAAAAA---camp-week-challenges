ef8510bbc54fd14447c39e93a972e48865310a56
H-1108 <sofiahkx1108@gmail.com>
2025-09-01 10:09:31 +0000

    .

--- challenge_day3/git_exercise.py
+++ challenge_day3/git_exercise.py
@@ -4,3 +4,10 @@ import liangyu1
 import maggie
 import tongfei
 import yangyang
+
+def team_name():
+    return "This is Team AAAAAA. We are:" + get_my_name()
+
+if __name__ == "__main__":
+    print(team_name())
+
