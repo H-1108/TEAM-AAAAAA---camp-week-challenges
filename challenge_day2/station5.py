43cf48967a74c8535dfa5322770957b1cf1b58fd
Yang02032 <yangyang02032@gmail.com>
2025-09-02 16:08:41 +0200

    Update station5.py

--- challenge_day2/station5.py
+++ challenge_day2/station5.py
@@ -1,41 +1,31 @@
-def solution_station_5(name: str):
+def solution_station_5(name: str) -> int:
+    # Define learning teams
     learning_teams = {
-        array1: [
+        1: [
             "Daeho", "David", "Kaisa", "Oliver", "Sara", "Dan", "Ivar", "Lotte", "Riya", "Vassil",
             "Twan", "Ester", "Karolina", "Lena", "Margarita", "Anna", "Kien", "Klaudia", "Maliah", "Todd"
         ],
-        array2: [
+        2: [
             "Oumaima", "Mathilde", "Marie", "Anita", "Ziyan", "Bernardo", "Eleanor", "Lorijn", "Maria",
             "Younes", "Yvan", "Henning", "Liangyu", "Maciej", "Toprak", "Chris", "GengXin", "Mingze", "Phoebe"
         ],
-        array3: [
+        3: [
             "Betija", "Haider", "Kacper", "Sophie", "Amir", "Baltasar", "Isar", "Jelle", "Nicolas", "David",
             "Ipek", "Juan", "Marfa", "Maria", "Alissa", "Leopoldo", "Mies", "Jiaying", "Kaixin", "Mai", "Sem", "Tibbe"
         ],
-        array4: [
+        4: [
             "Justus", "Julia", "Philip", "Uli", "Vanessa", "Anna", "Ekaterina", "Thessa", "Tongfei", "Yang",
             "Benedikt", "Jan", "Nadee", "Osjah", "Tim", "Eliana", "Joana", "Peilin", "Pija", "Wenhao"
         ],
-        array5: [
+        5: [
             "Afua", "Cristina", "Greta", "Jace", "Laura", "Anna", "Bassant", "Ivan", "Juriaan", "Kiavash"
         ],
-        array6: [
+        6: [
             "Keitaro", "Nohemi", "Norina", "Yifan", "Yinan", "Luo", "Nikola", "Olesya", "Sophie", "Tom"
         ]
     }
 
-    for x in range(0, 30):
-        if name == array1[x]:
-            print(1)
-        elif name == array2[x]:
-            print(2)
-        elif name == array3[x]:
-            print(3)
-        elif name == array4[x]:
-            print(4)
-        elif name == array5[x]:
-            print(5)
-        elif name == array6[x]:
-            print(6)
-        elif name == array7[x]:
-            print(7)
+    
+    for team_number, members in learning_teams.items():
+        if name in members:
+            return team_number
