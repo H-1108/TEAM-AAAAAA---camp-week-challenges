a98fe7cde3b7fc61d5679e77a7a0e568de1e0a87
Yang02032 <yangyang02032@gmail.com>
2025-09-02 14:13:34 +0200

    Update station2.py

--- challenge_day2/station2.py
+++ challenge_day2/station2.py
@@ -13,23 +13,24 @@ samples = [
 
 from datetime import date
 
-def date_to_japanese_weekday(s:str) -> str:
+def solution_station_2(s:str) -> str:
   weekdays = ["月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日", "日曜日"]
   d = date.fromisoformat(s)
   return weekdays[d.weekday()]
 
-assert all(date_to_japanese_weekday(x) == y for x, y in samples)
+assert all(solution_station_2(x) == y for x, y in samples)
 
 inputs = [
   "2024-03-22","2024-05-16","2024-12-18","2024-07-29","2024-12-02","2024-01-13",
   "2024-07-30","2024-05-01","2024-12-21","2024-07-27","2024-10-13",
 ]
 
-outputs = [date_to_japanese_weekday(s) for s in inputs]
+outputs = [solution_station_2(s) for s in inputs]
 result = dict(zip(inputs, outputs))
 
 if __name__ == "__main__":
     for s, out in zip(inputs, outputs):
         print(s, out)
+
 
 
