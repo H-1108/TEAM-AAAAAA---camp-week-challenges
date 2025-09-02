ed534667a98adc33125129e2447e979777a18e9b
H-1108 <sofiahkx1108@gmail.com>
2025-09-02 13:50:10 +0000

    updated

--- challenge_day2/station5.py
+++ challenge_day2/station5.py
@@ -1,16 +1,50 @@
-import re
+def solution_station_5():
+    learning_teams = {
+        "LT1": [
+            "Daeho", "David", "Kaisa", "Oliver", "Sara", "Dan", "Ivar", "Lotte", "Riya", "Vassil",
+            "Twan", "Ester", "Karolina", "Lena", "Margarita", "Anna", "Kien", "Klaudia", "Maliah", "Todd"
+        ],
+        "LT2": [
+            "Oumaima", "Mathilde", "Marie", "Anita", "Ziyan", "Bernardo", "Eleanor", "Lorijn", "Maria",
+            "Younes", "Yvan", "Henning", "Liangyu", "Maciej", "Toprak", "Chris", "GengXin", "Mingze", "Phoebe"
+        ],
+        "LT3": [
+            "Betija", "Haider", "Kacper", "Sophie", "Amir", "Baltasar", "Isar", "Jelle", "Nicolas", "David",
+            "Ipek", "Juan", "Marfa", "Maria", "Alissa", "Leopoldo", "Mies", "Jiaying", "Kaixin", "Mai", "Sem", "Tibbe"
+        ],
+        "LT4": [
+            "Justus", "Julia", "Philip", "Uli", "Vanessa", "Anna", "Ekaterina", "Thessa", "Tongfei", "Yang",
+            "Benedikt", "Jan", "Nadee", "Osjah", "Tim", "Eliana", "Joana", "Peilin", "Pija", "Wenhao"
+        ],
+        "LT5": [
+            "Afua", "Cristina", "Greta", "Jace", "Laura", "Anna", "Bassant", "Ivan", "Juriaan", "Kiavash"
+        ],
+        "LT6": [
+            "Keitaro", "Nohemi", "Norina", "Yifan", "Yinan", "Luo", "Nikola", "Olesya", "Sophie", "Tom"
+        ]
+    }
 
-def _normalize_name(name: str) -> str:
-    return re.sub(r"[^A-Za-z]", "", name).lower()
 
-def _pos(ch: str) -> int:
-    return ord(ch) - 96
+    name_to_team = {}
+    for team, members in learning_teams.items():
+        for person in members:
+            if person in name_to_team:
+                if isinstance(name_to_team[person], list):
+                    name_to_team[person].append(team)
+                else:
+                    name_to_team[person] = [name_to_team[person], team]
+            else:
+                name_to_team[person] = team
 
-def solution_station_5(name: str) -> int:
-    s = _normalize_name(name)
-    if not s:
-        return 0  
 
-    first = _pos(s[0])
-    last = _pos(s[-1])
-    return (3 * last - first) % 9
+    def find_learning_team(name: str):
+        if name in name_to_team:
+            return f"{name} belongs to {name_to_team[name]}"
+        else:
+            return f"{name} is not in any learning team."
+
+
+    print(find_learning_team("Betija"))
+    print(find_learning_team("Anna"))
+    print(find_learning_team("Tom"))
+    print(find_learning_team("Unknown"))
