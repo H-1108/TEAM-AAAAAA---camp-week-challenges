3b57f114df38b18b9989adf1b0ff34e322bc3a91
Yang02032 <yangyang02032@gmail.com>
2025-09-02 16:06:09 +0200

    Update station5.py

--- challenge_day2/station5.py
+++ challenge_day2/station5.py
@@ -1,39 +1,41 @@
 def solution_station_5(name: str):
     learning_teams = {
-        1: [
+        array1: [
             "Daeho", "David", "Kaisa", "Oliver", "Sara", "Dan", "Ivar", "Lotte", "Riya", "Vassil",
             "Twan", "Ester", "Karolina", "Lena", "Margarita", "Anna", "Kien", "Klaudia", "Maliah", "Todd"
         ],
-        2: [
+        array2: [
             "Oumaima", "Mathilde", "Marie", "Anita", "Ziyan", "Bernardo", "Eleanor", "Lorijn", "Maria",
             "Younes", "Yvan", "Henning", "Liangyu", "Maciej", "Toprak", "Chris", "GengXin", "Mingze", "Phoebe"
         ],
-        3: [
+        array3: [
             "Betija", "Haider", "Kacper", "Sophie", "Amir", "Baltasar", "Isar", "Jelle", "Nicolas", "David",
             "Ipek", "Juan", "Marfa", "Maria", "Alissa", "Leopoldo", "Mies", "Jiaying", "Kaixin", "Mai", "Sem", "Tibbe"
         ],
-        4: [
+        array4: [
             "Justus", "Julia", "Philip", "Uli", "Vanessa", "Anna", "Ekaterina", "Thessa", "Tongfei", "Yang",
             "Benedikt", "Jan", "Nadee", "Osjah", "Tim", "Eliana", "Joana", "Peilin", "Pija", "Wenhao"
         ],
-        5: [
+        array5: [
             "Afua", "Cristina", "Greta", "Jace", "Laura", "Anna", "Bassant", "Ivan", "Juriaan", "Kiavash"
         ],
-        6: [
+        array6: [
             "Keitaro", "Nohemi", "Norina", "Yifan", "Yinan", "Luo", "Nikola", "Olesya", "Sophie", "Tom"
         ]
     }
 
-    # Invert dictionary for quick lookup
-    name_to_team = {}
-    for team, members in learning_teams.items():
-        for person in members:
-            if person in name_to_team:
-                if isinstance(name_to_team[person], list):
-                    name_to_team[person].append(team)
-                else:
-                    name_to_team[person] = [name_to_team[person], team]
-            else:
-                name_to_team[person] = team
-
-    return name_to_team.get(name, None)
+    for x in range(0, 30):
+        if name == array1[x]:
+            print(1)
+        elif name == array2[x]:
+            print(2)
+        elif name == array3[x]:
+            print(3)
+        elif name == array4[x]:
+            print(4)
+        elif name == array5[x]:
+            print(5)
+        elif name == array6[x]:
+            print(6)
+        elif name == array7[x]:
+            print(7)
