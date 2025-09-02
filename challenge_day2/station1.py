7233b527ff6b66a56ad93bffb2ae1904d1e17b96
Yang02032 <yangyang02032@gmail.com>
2025-09-02 15:48:05 +0200

    Update station1.py

--- challenge_day2/station1.py
+++ challenge_day2/station1.py
@@ -1,13 +1,4 @@
 def solution_station_1(n: int) -> int:
-    """
-    It's Fibonacci number,
-    Station 1 inputs -> Fibonacci(input)
-        input = 4 -> output = 3
-        input = 10 -> output = 55
-        input = 98 -> output = 135301852344706740000
-
-
-"""
     if n < 0:
         raise ValueError("Input must be a non-negative integer")
     elif n == 0:
