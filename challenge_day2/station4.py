edcfa132f17b9fd19c2161e06286ce9646e6ba42
H-1108 <sofiahkx1108@gmail.com>
2025-09-02 12:35:32 +0000

    station4py updated

--- challenge_day2/station4.py
+++ challenge_day2/station4.py
@@ -6,23 +6,23 @@ def solution_station_4(x):
 
 
 #How I found the pattern:
-import pandas as pd 
-from sklearn.tree import DecisionTreeClassifier, export_text
+#import pandas as pd 
+#from sklearn.tree import DecisionTreeClassifier, export_text
 
-sample_input = [22, 73, 67, 89, 8, 42, 2, 21, 50, 2]
-sample_output = [False, True, True, True, False, False, True, False, False, True]
+#sample_input = [22, 73, 67, 89, 8, 42, 2, 21, 50, 2]
+#sample_output = [False, True, True, True, False, False, True, False, False, True]
 
-df = pd.DataFrame({"input": sample_input, "output": sample_output})
+#df = pd.DataFrame({"input": sample_input, "output": sample_output})
     
-input = df[["input"]]
-output = df["output"]
+#input = df[["input"]]
+#output = df["output"]
 
-tree_model = DecisionTreeClassifier()
-tree_model.fit(input, output)
+#tree_model = DecisionTreeClassifier()
+#tree_model.fit(input, output)
 
 #tree_rules = export_text(tree_model, feature_name = ["input"])
-tree_rules = export_text(tree_model)
-print("Pattern found \n", tree_rules)
+#tree_rules = export_text(tree_model)
+#print("Pattern found \n", tree_rules)
 
 
 #OUTPUT:  
