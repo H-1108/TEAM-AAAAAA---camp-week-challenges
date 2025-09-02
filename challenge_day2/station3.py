9f157b853ec944d6a4f475d66179561183e94cc6
gengxin0921 <gengxin0921@gmail.com>
2025-09-02 10:41:11 +0000

    WIP: station updates

---
+++ challenge_day2/station3.py
@@ -0,0 +1,3 @@
+def solution_station_3():
+    sample_input = [54, 83,58,14,12,30,89,79,86,89,21,24,10,78,88,40,62,15,]
+    sample_output = [True,False,False,False,True,True,False,False,False,False,True,True,False,True,False,False,False,True]
