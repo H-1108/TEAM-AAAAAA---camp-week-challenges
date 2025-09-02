b18f8042e5aeb095e1d5707294d99e62fa7aabb6
Yang02032 <yangyang02032@gmail.com>
2025-09-02 15:31:14 +0200

    Update station6.py

--- challenge_day2/station6.py
+++ challenge_day2/station6.py
@@ -1,5 +1,4 @@
 import math
 
-def solution_station_6(x: float, ndigits: int = 4) -> float:
-  rad = math.radians(x) if abs(x) > 2 * math.pi else x
-  return round(math.sin(rad), ndigits)
+def solution_station_6(x):
+    return round(math.sin(x),4)
