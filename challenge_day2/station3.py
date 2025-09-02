1941aa8dc8ad1163ec4aef00df2206fdaed71a67
gengxin0921 <gengxin0921@gmail.com>
2025-09-02 11:44:05 +0000

    hello

--- challenge_day2/station3.py
+++ challenge_day2/station3.py
@@ -1,3 +1,8 @@
-def solution_station_3():
+def solution_station_3(x):
+    if x <= 25 or x in{]54,78}:
+        return True
+    else:
+        return False
     sample_input = [54, 83,58,14,12,30,89,79,86,89,21,24,10,78,88,40,62,15,]
     sample_output = [True,False,False,False,True,True,False,False,False,False,True,True,False,True,False,False,False,True]
+    
