28b8f7370233f386a707aa58ab522017386edb34
Yang02032 <yangyang02032@gmail.com>
2025-09-02 15:58:15 +0200

    Update station5.py

--- challenge_day2/station5.py
+++ challenge_day2/station5.py
@@ -1,30 +1,30 @@
-def solution_station_5():
+def solution_station_5(name: str):
     learning_teams = {
-        "LT1": [
+        1: [
             "Daeho", "David", "Kaisa", "Oliver", "Sara", "Dan", "Ivar", "Lotte", "Riya", "Vassil",
             "Twan", "Ester", "Karolina", "Lena", "Margarita", "Anna", "Kien", "Klaudia", "Maliah", "Todd"
         ],
-        "LT2": [
+        2: [
             "Oumaima", "Mathilde", "Marie", "Anita", "Ziyan", "Bernardo", "Eleanor", "Lorijn", "Maria",
             "Younes", "Yvan", "Henning", "Liangyu", "Maciej", "Toprak", "Chris", "GengXin", "Mingze", "Phoebe"
         ],
-        "LT3": [
+        3: [
             "Betija", "Haider", "Kacper", "Sophie", "Amir", "Baltasar", "Isar", "Jelle", "Nicolas", "David",
             "Ipek", "Juan", "Marfa", "Maria", "Alissa", "Leopoldo", "Mies", "Jiaying", "Kaixin", "Mai", "Sem", "Tibbe"
         ],
-        "LT4": [
+        4: [
             "Justus", "Julia", "Philip", "Uli", "Vanessa", "Anna", "Ekaterina", "Thessa", "Tongfei", "Yang",
             "Benedikt", "Jan", "Nadee", "Osjah", "Tim", "Eliana", "Joana", "Peilin", "Pija", "Wenhao"
         ],
-        "LT5": [
+        5: [
             "Afua", "Cristina", "Greta", "Jace", "Laura", "Anna", "Bassant", "Ivan", "Juriaan", "Kiavash"
         ],
-        "LT6": [
+        6: [
             "Keitaro", "Nohemi", "Norina", "Yifan", "Yinan", "Luo", "Nikola", "Olesya", "Sophie", "Tom"
         ]
     }
 
-
+    # Invert dictionary for quick lookup
     name_to_team = {}
     for team, members in learning_teams.items():
         for person in members:
@@ -36,15 +36,4 @@ def solution_station_5():
             else:
                 name_to_team[person] = team
 
-
-    def find_learning_team(name: str):
-        if name in name_to_team:
-            return f"{name} belongs to {name_to_team[name]}"
-        else:
-            return f"{name} is not in any learning team."
-
-
-    print(find_learning_team("Betija"))
-    print(find_learning_team("Anna"))
-    print(find_learning_team("Tom"))
-    print(find_learning_team("Unknown"))
+    return name_to_team.get(name, None)
